1c71f1131f171acf9b025e3ae931dcafa8ac4958
DDJ <pwe1ewq@gmail.com>
2024-06-19 22:09:44 +0900

    순수 자바 코드 -> 스프링 사용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,28 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import hello.core.order.RateDiscountPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberSeriveImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), dIscountPolicy());
     }
 
+    @Bean
     public DIscountPolicy dIscountPolicy(){
         return new RateDiscountPolicy();
     }
